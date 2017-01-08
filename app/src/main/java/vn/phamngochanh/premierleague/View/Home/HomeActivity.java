65df41ac20763dd9983618da110920968872b07c
PhamNgocHanh <hanhthoi0412@gmail.com>
2017-01-08 21:59:41 +0700

    commit

--- app/src/main/java/vn/phamngochanh/premierleague/View/Home/HomeActivity.java
+++ app/src/main/java/vn/phamngochanh/premierleague/View/Home/HomeActivity.java
@@ -2,7 +2,12 @@ package vn.phamngochanh.premierleague.View.Home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import vn.phamngochanh.premierleague.R;
 
@@ -12,9 +17,31 @@ import vn.phamngochanh.premierleague.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return true;
     }
 }
